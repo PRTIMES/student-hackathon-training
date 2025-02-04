6339bd055b11b04c8e819a3db39edb1ff128cfde
kirikirisu <kouhei.kirisawa@prtimes.co.jp>
2025-02-04 15:07:05 +0900

    Step2: create todo

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -1,16 +1,46 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+type TodoItem = {
+  text: string;
+};
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState<TodoItem[]>([]);
 
   return (
     <>
       <h1>TODOアプリ</h1>
       <div className="inputForm">
-        <input className="input" type="text" />
-        <button className="addButton" type="button">追加</button>
+        <input
+          className="input"
+          type="text"
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+        />
+        <button
+          className="addButton"
+          type="button"
+          onClick={() => {
+            if (inputValue === "") return
+
+            setTodoList((prev) => [...prev, { text: inputValue }]);
+            setInputValue("");
+          }}
+        >
+          追加
+        </button>
       </div>
+      <ul>
+        {todoList.map((todo) => {
+          return <li key={todo.text}>{todo.text}</li>;
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
