212502c4b4e7f0701be1db6e860dae12cf2f56bc
kirikirisu <kouhei.kirisawa@prtimes.co.jp>
2025-02-04 14:35:15 +0900

    Step1: add text input & add todo button

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <h1>TODOアプリ</h1>
+      <div className="inputForm">
+        <input className="input" type="text" />
+        <button className="addButton" type="button">追加</button>
+      </div>
     </>
   )
 }
