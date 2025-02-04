87b3c4644944228226f1b17177dc791c27e04f5a
kirikirisu <kouhei.kirisawa@prtimes.co.jp>
2025-02-04 16:38:54 +0900

    Step3: complete todo

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 type TodoItem = {
+  id: string;
   text: string;
+  isCompleted: boolean;
 };
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
+
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted)
 
   return (
     <>
@@ -25,18 +30,35 @@ function App() {
           className="addButton"
           type="button"
           onClick={() => {
-            if (inputValue === "") return
+            if (inputValue === "") return;
 
-            setTodoList((prev) => [...prev, { text: inputValue }]);
+            setTodoList((prev) => [
+              ...prev,
+              { id: uuidv4(), text: inputValue, isCompleted: false },
+            ]);
             setInputValue("");
           }}
         >
           追加
         </button>
       </div>
-      <ul>
-        {todoList.map((todo) => {
-          return <li key={todo.text}>{todo.text}</li>;
+      <ul className="todoList">
+        {filteredTodoList.map((todo) => {
+          return (
+            <div className="todoItem">
+              <li key={todo.text}>{todo.text}</li>
+              <button className="completeButton" type="button" onClick={() => {
+                setTodoList((prev) => {
+                  return prev.map((item) => {
+                    if (item.id === todo.id) {
+                      return { ...item, isCompleted: true};
+                    }
+                    return item;
+                  });
+                }) 
+              }}>完了</button>
+            </div>
+          );
         })}
       </ul>
     </>
