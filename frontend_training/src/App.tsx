ba0b59f6b9cbd68a5ce249b29493a2857ce4b607
kirikirisu <kouhei.kirisawa@prtimes.co.jp>
2025-02-04 17:36:39 +0900

    Step4: update todo

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -6,13 +6,14 @@ type TodoItem = {
   id: string;
   text: string;
   isCompleted: boolean;
+  isEdit: boolean;
 };
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
 
-  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted)
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
 
   return (
     <>
@@ -34,7 +35,12 @@ function App() {
 
             setTodoList((prev) => [
               ...prev,
-              { id: uuidv4(), text: inputValue, isCompleted: false },
+              {
+                id: uuidv4(),
+                text: inputValue,
+                isCompleted: false,
+                isEdit: false,
+              },
             ]);
             setInputValue("");
           }}
@@ -44,19 +50,44 @@ function App() {
       </div>
       <ul className="todoList">
         {filteredTodoList.map((todo) => {
+          if (todo.isEdit) {
+            return <EditTodo todo={todo} setTodoList={setTodoList} />;
+          }
+
           return (
             <div className="todoItem">
-              <li key={todo.text}>{todo.text}</li>
-              <button className="completeButton" type="button" onClick={() => {
-                setTodoList((prev) => {
-                  return prev.map((item) => {
-                    if (item.id === todo.id) {
-                      return { ...item, isCompleted: true};
-                    }
-                    return item;
+              <li
+                key={todo.text}
+                onClick={() => {
+                  setTodoList((prev) => {
+                    return prev.map((item) => {
+                      if (item.id === todo.id) {
+                        return { ...item, isEdit: true };
+                      }
+
+                      return item;
+                    });
                   });
-                }) 
-              }}>完了</button>
+                }}
+              >
+                {todo.text}
+              </li>
+              <button
+                className="completeButton"
+                type="button"
+                onClick={() => {
+                  setTodoList((prev) => {
+                    return prev.map((item) => {
+                      if (item.id === todo.id) {
+                        return { ...item, isCompleted: true };
+                      }
+                      return item;
+                    });
+                  });
+                }}
+              >
+                完了
+              </button>
             </div>
           );
         })}
@@ -66,3 +97,57 @@ function App() {
 }
 
 export default App;
+
+function EditTodo({
+  todo,
+  setTodoList,
+}: {
+  todo: TodoItem;
+  setTodoList: React.Dispatch<React.SetStateAction<TodoItem[]>>;
+}) {
+  const [inputValue, setInputValue] = useState(todo.text);
+
+  return (
+    <div className="editItem">
+      <input
+        value={inputValue}
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+        className="input"
+      />
+      <div className="actionButtons">
+        <button className="updateButton" type="button" onClick={() => {
+          setTodoList((prev) => {
+            return prev.map((item) => {
+              if (item.id === todo.id) {
+                return { ...item, text: inputValue, isEdit: false };
+              }
+
+              return item;
+            });
+          });
+        }}>
+          更新
+        </button>
+        <button
+          className="cancelButton"
+          type="button"
+          onClick={() => {
+            setTodoList((prev) => {
+              return prev.map((item) => {
+                if (item.id === todo.id) {
+                  return { ...item, isEdit: false };
+                }
+
+                return item;
+              });
+            });
+          }}
+        >
+          キャンセル
+        </button>
+      </div>
+    </div>
+  );
+}
